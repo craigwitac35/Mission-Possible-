a4128112814c35a0c6fffc42c233f2faad4bac13
craigwitac35 <spacegook101@gmail.com>
2026-05-09 14:52:10 -0500

    Enhance App component with font preconnect links
    Added Head component for font preloading.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,500;0,9..144,600;0,9..144,700;1,9..144,400;1,9..144,500&family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
