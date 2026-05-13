34e6721175f8d8c5376dfccc3b329b5ec22b5819
craigwitac35 <spacegook101@gmail.com>
2026-05-13 17:47:28 -0500

    Remove StaffLoginIcon from events page

--- pages/events.tsx
+++ pages/events.tsx
@@ -15,7 +15,6 @@ export default function Events() {
       </Head>
 
       <main className="mp-site">
-        <StaffLoginIcon />
         <PublicSiteHeader />
 
         <section className="mp-page-hero-art-v2">
